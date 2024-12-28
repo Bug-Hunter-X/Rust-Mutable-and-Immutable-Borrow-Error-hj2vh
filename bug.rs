6c3e5e462ad691aacecdcc307677a7022cd72eca
BugHunter-X <askzenai@gmail.com>
2024-12-28 03:56:04 +0100

    Add bug.rs file

---
+++ bug.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut x = 5;
+    let y = &mut x; // y is a mutable reference to x
+    let z = &x;    // z is an immutable reference to x
+
+    *y += 1; // Modifying x through y is allowed
+    println!("x = {}", x); // Prints x = 6
+
+    // println!("z = {}", *z); // This will cause a compile-time error.
+}
